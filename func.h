d22e231eab08fede0d252ebe0b5286ec0cc5109d
kwibber <vadim.popov0660@gmail.com>
2025-09-28 19:44:14 +0300

    new

---
+++ func.h
@@ -0,0 +1,25 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+
+
+double circumference_length(Circle &p);
+double circle_area(Circle &p);
+double perimeter(Square &p);
+double square_area(Square &p);
+void read_console(Point &p);
+void read_console_circle(Circle &p);
+void read_console_square(Square &p);
+void export_console(Point &p);
+void export_console_circle(Circle &p);
+void export_console_square(Square &p);
+bool point_affiliation_circle(Point &a, Circle &b);
+bool point_affiliation_square(Point &a, Square &c);
+bool point_on_line_circle(Point &a, Circle &b);
+bool point_on_line_square(Point &a, Square &c);
+bool Intersection_2_circle(Circle &a, Circle &b);
+bool Intersection_2_square(Square &a, Square &b);
+bool Intersection_square_circle(Square &a, Circle &b);
+
+
+ #endif // FUNC_H
