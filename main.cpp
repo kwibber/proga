b45d2c948721be6b08d09a55a2301accd00900e3
kwibber <vadim.popov0660@gmail.com>
2025-09-29 20:39:43 +0300

    4

--- main.cpp
+++ main.cpp
@@ -16,10 +16,26 @@ int main(){
     std::cout << "X, Y, lenght to square: ";
     read_console_square(s);
     export_console_square(s);
-    point_affiliation_circle(p, c);
-    point_affiliation_square(p, s);
-    point_on_line_circle(p, c);
-    point_on_line_square(p, s);
+    if (point_affiliation_circle(p, c)){
+        std::cout << "point affiliation circle" << std::endl;
+    } else {
+        std::cout << "point not affiliation circle" << std::endl;
+    }
+    if (point_affiliation_square(p, s)){
+        std::cout << "point affiliation square" << std::endl;
+    } else {
+        std::cout << "point not affiliation square" << std::endl;
+    }
+    if (point_on_line_circle(p, c)){
+        std::cout << "point on line circle" << std::endl;
+    } else {
+        std::cout << "point not on line circle" << std::endl;
+    }
+    if (point_on_line_square(p, s)){
+        std::cout << "point on line square" << std::endl;
+    } else {
+        std::cout << "point not on line square" << std::endl;
+    }
     Circle c1;
     std::cout << "X, Y, R to first circle: ";
     read_console_circle(c1);
@@ -36,7 +52,19 @@ int main(){
     std::cout << "X, Y, lenght to second square: ";
     read_console_square(s2);
     export_console_square(s2);
-    Intersection_2_circle(c1, c2);
-    Intersection_2_square(s1, s2);
-    Intersection_square_circle(s1, c1);
+    if (Intersection_2_circle(c1, c2)){
+        std::cout << "2 circles intersect" << std::endl;
+    } else {
+        std::cout << "2 circles not intersect" << std::endl;
+    }
+    if (Intersection_2_square(s1, s2)){
+        std::cout << "2 squares intersect" << std::endl;
+    } else {
+        std::cout << "2 squares not intersect" << std::endl;
+    }
+    if (Intersection_square_circle(s1, c1)){
+        std::cout << "square and circle intersect" << std::endl;
+    } else {
+        std::cout << "square and circle not intersect" << std::endl;
+    }
 }
